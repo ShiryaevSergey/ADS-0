0d81db59ab96a9c2aa9d8fe293521288316bb34b
puzhx <99177486+ShiryaevSergey@users.noreply.github.com>
2022-02-11 00:27:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,21 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    
-    int k=0;   
-    if (a>b) {
-        for (int i=b; i>0; --i) {
-            if ((a%i==0) && (b%i==0)) {
-                k=i;
+int k = 0;   
+    if (a > b) {
+        for (int i = b; i > 0; --i) {
+            if ((a % i == 0) && (b % i == 0)) {
+                k = i;
                 break;
-            }
-            
+            }      
         }
     } else {
-        for (int i=a; i>0; --i) {
-            if ((a%i==0) && (b%i==0)) {
-                k=i;
+        for (int i = a; i > 0; --i) {
+            if ((a % i == 0) && (b % i == 0)) {
+                k = i;
                 break;
-            }
-            
+            } 
         }
     }
-    
-    
-    
     return k;
-
 }
