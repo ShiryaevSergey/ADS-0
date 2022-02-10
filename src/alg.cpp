b21b3f1336f2606bc8beea7821cba2e26c1ad7fe
puzhx <99177486+ShiryaevSergey@users.noreply.github.com>
2022-02-10 23:23:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,27 @@
 // Copyright 2021 NNTU-CS
 
-int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+int gcd(int a, int b) {   
+    k=0;   
+    if (a>b) {
+        for (int i=b; i>0; --i) {
+            if ((a%i==0) && (b%i==0)) {
+                k=i;
+                break;
+            }
+            
+        }
+    } else {
+        for (int i=a; i>0; --i) {
+            if ((a%i==0) && (b%i==0)) {
+                k=i;
+                break;
+            }
+            
+        }
+    }
+    
+    
+    
+    return k;
+
 }
